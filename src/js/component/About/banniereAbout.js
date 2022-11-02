d084da559c93346fae2d61f0102acffe49e1da43
joncoder <joncoder.kamite@gmail.com>
2022-11-02 22:04:38 +0100

    Updates

--- src/js/component/About/banniereAbout.js
+++
@@ -1,7 +0,0 @@
-function BanniereAbout() {
-    return (
-        <div className="a-propos"></div>
-    ) ;
-}
-
-export default BanniereAbout;
--- src/js/component/Home/banniere.js
+++
@@ -1,9 +0,0 @@
-function Banniere() {
-    return (
-        <section className="banniere">
-            <h1 className="slogan">Chez vous, partout et ailleurs</h1>
-        </section>
-    );    
-}
-
-export default Banniere;
