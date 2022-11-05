6d8dd138c2fd780c91af5c2a29eb38d042aaea39
joncoder <joncoder.kamite@gmail.com>
2022-11-05 09:54:40 +0100

    mise de toute la logique de routage dans le fichier

--- src/js/component/main/app.js
+++ src/js/component/main/app.js
@@ -1,14 +1,24 @@
 import Header from './header';
-import MainChanges from "./mainChanges";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './footer';
-import { BrowserRouter } from 'react-router-dom';
 import { Component } from 'react';
+import Home from '../../page/Home';
+import About from '../../page/About';
+import Logement from '../../page/Logement';
+import ErrorPage from '../../page/Error';
 
 function AppRender() {
     return (
         <BrowserRouter>
             <Header />
-            <MainChanges />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/kasa-react" element={<Home />} />
+                <Route path="/kasa-react/apropos" element={<About />} />
+                <Route path="/kasa-react/logement/:id" element={<Logement />} />
+                <Route path="/*" element={<ErrorPage />}/>
+                <Route path="/kasa-react/*" element={<ErrorPage />}/>
+            </Routes>
             <Footer />
         </BrowserRouter>
     )
